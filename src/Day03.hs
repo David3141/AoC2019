c3df2a1f1a844464c3cf176f07e9f7b5b0fe57cb
David L <david.landry@veldrane.de>
2020-01-03 21:50:50 +0100

    Improve Day03 part 1 by using sequences instead of lists
    - boosts performance by a ton

--- src/Day03.hs
+++ src/Day03.hs
@@ -7,6 +7,13 @@ where
 import           Paths_advent_of_code
 
 import qualified Data.Set                      as Set
+import qualified Data.Sequence                 as Seq
+import           Data.Sequence                  ( Seq
+                                                , Seq((:<|))
+                                                , (><)
+                                                , ViewR((:>))
+                                                )
+import           Data.Foldable                  ( toList )
 
 import           Helpers                        ( readCommaSeparatedStrings )
 
@@ -17,16 +24,39 @@ type Path = [Node]
 
 part1 :: IO Int
 part1 = do
+  print "-"
+
   (instructionsA, instructionsB) <- readPathInstructions
 
-  let pathA         = pathFromInstructions instructionsA
-  let pathB         = pathFromInstructions instructionsB
+  -- let pathA         = pathFromInstructions instructionsA
+  -- let pathB         = pathFromInstructions instructionsB
 
-  let intersections = Set.intersection pathA pathB
-  let minimumDistance =
-        minimum . Set.map (manhattanDistance (0, 0)) $ intersections
+  -- print $ "Set 1, length " ++ show (Set.size pathA)
+  -- print $ Set.take 5 pathA
 
-  return minimumDistance
+  -- print $ "Set 2, length " ++ show (Set.size pathB)
+  -- print $ Set.take 5 pathB
+
+  -- let intersections = Set.intersection pathA pathB
+
+  -- print intersections
+
+  -- let minimumDistance =
+  --       minimum . Set.map (manhattanDistance (0, 0)) $ intersections
+
+  -- return minimumDistance
+
+  -- Due to how I implemented this, the first two elements are the root node.
+  let (_ :<| _ :<| pathA2) = pathFromInstructions2 instructionsA
+  let (_ :<| _ :<| pathB2) = pathFromInstructions2 instructionsB
+
+  let intersections2 = Set.intersection (Set.fromList $ toList pathA2)
+                                        (Set.fromList $ toList pathB2)
+
+  let minimumDistance2 =
+        minimum . Set.map (manhattanDistance (0, 0)) $ intersections2
+
+  return minimumDistance2
 
 
 part2 :: IO Int
@@ -43,28 +73,58 @@ readPathInstructions = do
   return (firstPath, secondPath)
 
 
-pathFromInstruction :: Node -> PathInstruction -> Path
-pathFromInstruction (startX, startY) (direction : strDistance) = zip xs ys
+-- pathFromInstruction :: Node -> PathInstruction -> Path
+-- pathFromInstruction (startX, startY) (direction : strDistance) = zip xs ys
+--  where
+--   distance = read strDistance
+--   xs       = case direction of
+--     'U' -> repeat startX
+--     'D' -> repeat startX
+--     'R' -> [startX .. startX + distance]
+--     'L' -> [startX, pred startX .. startX - distance]
+--   ys = case direction of
+--     'U' -> [startY .. startY + distance]
+--     'D' -> [startY, pred startY .. startY - distance]
+--     'R' -> repeat startY
+--     'L' -> repeat startY
+
+pathFromInstruction2 :: Node -> PathInstruction -> Seq Node
+pathFromInstruction2 (startX, startY) (direction : strDistance) = Seq.zip xs ys
  where
   distance = read strDistance
   xs       = case direction of
-    'U' -> repeat startX
-    'D' -> repeat startX
-    'R' -> [startX .. startX + distance]
-    'L' -> [startX, pred startX .. startX - distance]
+    'U' -> Seq.replicate distance startX
+    'D' -> Seq.replicate distance startX
+    'R' -> Seq.fromFunction distance (+ succ startX)
+    'L' -> Seq.fromFunction distance ((-) $ pred startX)
+    -- 'R' -> Seq.fromList [startX + 1 .. startX + distance]
+    -- 'L' -> Seq.fromList [startX - 1, startX - 2 .. startX - distance]
   ys = case direction of
-    'U' -> [startY .. startY + distance]
-    'D' -> [startY, pred startY .. startY - distance]
-    'R' -> repeat startY
-    'L' -> repeat startY
+    'U' -> Seq.fromFunction distance (+ succ startY)
+    'D' -> Seq.fromFunction distance ((-) $ pred startY)
+    -- 'U' -> Seq.fromList [startY + 1 .. startY + distance]
+    -- 'D' -> Seq.fromList [startY - 1, startY - 2 .. startY - distance]
+    'R' -> Seq.replicate distance startY
+    'L' -> Seq.replicate distance startY
 
 
-pathFromInstructions :: [PathInstruction] -> Set.Set Node
-pathFromInstructions = Set.delete (0, 0) . Set.fromList . foldl
-  (\acc curr -> acc ++ pathFromInstruction (last acc) curr)
-  [(0, 0)]
+pathFromInstructions2 :: [PathInstruction] -> Seq Node
+pathFromInstructions2 = foldl
+  (\acc curr -> acc >< pathFromInstruction2 (lastThing acc) curr)
+  (Seq.singleton (0, 0))
+
+-- pathFromInstructions :: [PathInstruction] -> Set.Set Node
+-- pathFromInstructions = Set.delete (0, 0) . Set.fromList . foldl
+--   (\acc curr -> acc ++ pathFromInstruction (last acc) curr)
+--   [(0, 0)]
 
 
 manhattanDistance :: Node -> Node -> Int
 manhattanDistance (startX, startY) (x, y) =
   (abs x - abs startX) + (abs y - abs startY)
+
+
+lastThing :: Seq a -> a
+lastThing seq = case Seq.viewr seq of
+  Seq.EmptyR -> error "no end"
+  as :> a    -> a
